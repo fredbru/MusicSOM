903f10d5011d7517e8ba4387850db3c12f2dd057
Fred Bruford <fred.bruford@gmail.com>
2019-05-04 21:54:12 +0100

    added edit distances

--- Get-All-Similarity.py
+++ Get-All-Similarity.py
@@ -2,53 +2,75 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 import csv
+import editdistance as edcalc
 
 allGrooves = np.load('Eval-matricies.npy')
 allNames = np.load('Eval-names.npy')
 
-def getEuclideanDistance(grooveA, grooveB):
+def getEuclideanDistance(a, b):
     """Returns norm-2 of a 1-D numpy array for CPU computation.
     * faster than linalg.norm in case of 1-D arrays (numpy 1.9.2rc1).
     """
-    x = (grooveA.flatten()-grooveB.flatten())
+    x = (a.flatten()-b.flatten())
     return math.sqrt(np.dot(x, x.T))
 
-def getHammingDistance(grooveA, grooveB):
+def getHammingDistance(a, b):
     # Same as euclidean, without velocity
-    binaryA = np.ceil(grooveA).flatten()
-    binaryB = np.ceil(grooveB).flatten()
+    binaryA = np.ceil(a).flatten()
+    binaryB = np.ceil(b).flatten()
     return np.count_nonzero(binaryA != binaryB)
 
-def getDirectedSwapDistance(grooveA, grooveB):
+def getDirectedSwapDistance(a, b):
     # Directed swap: swap but for rhythms with variable #of onsets. Uses binary rhythms.
-    binaryA = np.ceil(grooveA).flatten()
-    binaryB = np.ceil(grooveB).flatten()
-
+    binaryA = np.ceil(a).flatten()
+    binaryB = np.ceil(b).flatten()
     pass
 
+def getEditDistance(a,b):
+    # get edit distance no velocity
+    binaryA = np.ceil(a)
+    binaryB = np.ceil(b)
+    aKick, aSnare, aClosed, aOpen, aTom = splitKitParts(binaryA)
+    bKick, bSnare, bClosed, bOpen, bTom = splitKitParts(binaryB)
+    combinedEditDistance = edcalc.eval(aKick,bKick) + edcalc.eval(aSnare,bSnare) + \
+                           edcalc.eval(aClosed,bClosed) + edcalc.eval(aOpen,bOpen) + \
+                           edcalc.eval(aTom,bTom)
+    return combinedEditDistance
+
+def splitKitParts(groove):
+    kick = groove[:,0]
+    snare = groove[:,1]
+    closed = groove[:,2]
+    open = groove[:,3]
+    tom = groove[:,4]
+    return kick, snare, closed, open, tom
 
 allEuclideanDistances = []
 allHammingDistances = []
+allEditDistances = []
 j=0
 with open("/home/fred/BFD/python/Similarity-Eval/eval-pairings.csv") as csvfile:
     reader = csv.reader(csvfile, delimiter=",")
     for row in reader:
-        grooveAName = row[0]
-        grooveBName = row[1]
+        aName = row[0]
+        bName = row[1]
         for i in range(len(allNames)):
-            if allNames[i] == grooveAName:
-                grooveA = allGrooves[i]
-            if allNames[i] == grooveBName:
-                grooveB = allGrooves[i]
-        euclideanRhythmDistance = getEuclideanDistance(grooveA,grooveB)
-        hammingDistance = getHammingDistance(grooveA,grooveB)
+            if allNames[i] == aName:
+                a = allGrooves[i]
+            if allNames[i] == bName:
+                b = allGrooves[i]
+        euclideanRhythmDistance = getEuclideanDistance(a,b)
+        hammingDistance = getHammingDistance(a,b)
+        editdistance = getEditDistance(a,b)
 
         allEuclideanDistances.append(euclideanRhythmDistance)
         allHammingDistances.append(hammingDistance)
+        allEditDistances.append(editdistance)
 
-        print(grooveAName + "  " + grooveBName + '    Euclidean = ' + str(euclideanRhythmDistance) + "Hamming = "
+        print(aName + "  " + bName + '    Euclidean = ' + str(euclideanRhythmDistance) + "Hamming = "
               + str(hammingDistance))
         j=j+1
+
 plt.figure()
 plt.hold(True)
 plt.bar(np.arange(100),allEuclideanDistances)
@@ -57,4 +79,8 @@ plt.title("Euclidean Distances")
 plt.figure()
 plt.bar(np.arange(100),allHammingDistances)
 plt.title("Hamming Distances")
+
+plt.figure()
+plt.bar(np.arange(100),allEditDistances)
+plt.title("Edit Distances")
 plt.show()
--- Get-Similarity.py
+++ Get-Similarity.py
@@ -1,32 +1,64 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import math
+import editdistance
+
+
+def getEuclideanDistance(A, B):
+    """Returns norm-2 of a 1-D numpy array for CPU computation.
+    * faster than linalg.norm in case of 1-D arrays (numpy 1.9.2rc1).
+    """
+    x = (A.flatten()-B.flatten())
+    return math.sqrt(np.dot(x, x.T))
+
+
+def getDirectedSwapDistance(A, B):
+    # Directed swap: swap but for rhythms with variable #of onsets. Uses binary rhythms.
+    binaryA = np.ceil(A)
+    binaryB = np.ceil(B)
+    # aKick, aSnare, aClosed, aOpen, aTom = splitKitParts(binaryA)
+    # bKick, bSnare, bClosed, bOpen, bTom = splitKitParts(binaryB)
+
+def getEditDistance(A,B):
+    # get edit distance no velocity
+    binaryA = np.ceil(A)
+    binaryB = np.ceil(B)
+    aKick, aSnare, aClosed, aOpen, aTom = splitKitParts(binaryA)
+    bKick, bSnare, bClosed, bOpen, bTom = splitKitParts(binaryB)
+    combinedEditDistance = editdistance.eval(aKick,bKick) + editdistance.eval(aSnare+bSnare) + \
+                           editdistance.eval(aClosed+bClosed) + editdistance.eval(aOpen,bOpen) + \
+                           editdistance.eval(aTom+bTom)
+    print(combinedEditDistance)
+
+def splitKitParts(groove):
+    kick = groove[:,0]
+    snare = groove[:,1]
+    closed = groove[:,2]
+    open = groove[:,3]
+    tom = groove[:,4]
+    return kick, snare, closed, open, tom
+
+def calculateMonoSwapDistance(partA, partB):
+    if partA.sum() > partB.sum():
+        pass
+
+def testMonoEditDistance():
+    A = np.array([1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+    B = np.array([1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0])
+    print(editdistance.eval(A, B)) #should = 12
+
+    son = np.array([1,0,0,1,0,0,1,0,0,0,1,0,1,0,0,0])
+    shiko = np.array([1,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0])
+    print(editdistance.eval(son, shiko)) #should = 2
+
 
 evalGrooves = np.load('Eval-matricies.npy')
 evalNames = np.load('Eval-names.npy')
 
 for i in range(evalNames.shape[0]):
     if 'Blues CH1 a' in evalNames[i]:
-        grooveA = evalGrooves[i]
+        A = evalGrooves[i]
     if 'Blues CH1 b' in evalNames[i]:
-        grooveB = evalGrooves[i]
+        B = evalGrooves[i]
 
-def getEuclideanDistance(grooveA, grooveB):
-    """Returns norm-2 of a 1-D numpy array for CPU computation.
-    * faster than linalg.norm in case of 1-D arrays (numpy 1.9.2rc1).
-    """
-    x = (grooveA.flatten()-grooveB.flatten())
-    return math.sqrt(np.dot(x, x.T))
-
-
-def getDirectedSwapDistance(grooveA, grooveB):
-    # Directed swap: swap but for rhythms with variable #of onsets. Uses binary rhythms.
-    binaryA = np.ceil(grooveA).flatten()
-    binaryB = np.ceil(grooveB).flatten()
-    if binaryA.sum() > binaryB.sum(): #find which one has the most onsets
-        pass
-    else:
-
-    pass
-
-d = getDirectedSwapDistance(grooveA,grooveB)
+print(getEditDistance())
